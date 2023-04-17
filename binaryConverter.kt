5f458f08f399e33dec74d2699827a81c67577cff
g3th <theinkandpen@yahoo.co.uk>
2023-04-17 18:50:53 +0100

    rewrite, added dec to Binary, added isSymbol error check 	modified:   binaryConverter.kt

--- binaryConverter.kt
+++ binaryConverter.kt
@@ -2,7 +2,18 @@ fun clearScreen(){
     ProcessBuilder("clear").redirectOutput(ProcessBuilder.Redirect.INHERIT).start().waitFor()
 }
 
-fun invalidValue(string: String) : Boolean {
+fun isSymbol(string: String) : Boolean{
+    val symbols = "'.&<>'`*@\\{}[]^¢« »©†°÷[[ ]]…—–=€!`>≥-\"<≤–×≠#¶( )%π|+±?®§/~™_"
+    for (i in symbols){
+        for (j in string){
+            if(j == i){
+               return true
+            }
+        }
+    }
+    return false
+}
+fun checkValue(string: String) : String {
     val valueList = mutableListOf<Int>()
     string.forEach {
         valueList.add( (it.code - '0'.code) )
@@ -10,11 +21,41 @@ fun invalidValue(string: String) : Boolean {
     for (i in valueList){
         if (i >1){
             clearScreen()
-            println("Invalid Binary values (contains numbers larger than 1s or 0s).")
-            return true
+            return "fromDecimalToBinary"
         }
     }
-    return false
+    return "fromBinaryToDecimal"
+}
+
+fun binaryToDecConverter(binaryNumber: String) : Long {
+    var counter = 0
+    val newNum = mutableListOf<Long>()
+    val index = binaryNumber.length-1.toLong()
+    for (i in index downTo 0) {
+        newNum.add((binaryNumber[counter].code - '0'.code) * powerOf(2, i))
+        counter += 1
+    }
+    return newNum.sum()
+}
+
+fun decToBinaryConverter(num: Int) : String {
+    var convertedNum : Double = num.toDouble()
+    var invertedResult = ""
+    var finalResult = ""
+
+    do {
+        convertedNum /= 2
+        if (convertedNum.toString().contains(".5")) {
+            invertedResult += "1"
+        } else {
+            invertedResult += "0"
+        }
+        convertedNum = convertedNum.toInt().toDouble()
+    } while (convertedNum != 0.0)
+    for (i in invertedResult.length-1 downTo 0){
+        finalResult += (invertedResult[i])
+    }
+    return finalResult
 }
 
 fun powerOf(num1: Long, num2: Long) : Long {
@@ -24,34 +65,40 @@ fun powerOf(num1: Long, num2: Long) : Long {
         return 1
     }
     while (counter < num2){
-        result = result * num1
+        result *= num1
         counter += 1
     }
     return result
 }
+
 fun main(){
-	clearScreen()
+
+    clearScreen()
+
     var errorFlag = true
-    var num = ""
 
     while (errorFlag) {
+        
         println("Enter a Binary Number (1s - 0s): ")
-        num = readln()
-        if (num.all { it.isLetter()}){
+
+        val num = readln()
+        val requiredConversion = checkValue(num)
+
+        if (num.all {it.isLetter()}) {
             clearScreen()
             println("Invalid Binary values (letters).")
-        } else if (!invalidValue(num)){
+        } else if (isSymbol(num)){
+            clearScreen()
+            println("Invalid Binary values (symbols).")
+        } else {
+            if (requiredConversion == "fromDecimalToBinary") {
+                println("\nThe Decimal Number Entered: $num")
+                println("Converts Into the Binary Number: ${decToBinaryConverter(num.toInt())}\n")
+            } else {
+                println("\nThe Binary Number Entered: $num")
+                println("Converts Into the Decimal Number: ${binaryToDecConverter(num)}\n")
+            }
             errorFlag = false
         }
     }
-
-    var counter = 0
-    val newNum = mutableListOf<Long>()
-    val index = num.length-1.toLong()
-    for (i in index downTo 0){
-        newNum.add( (num[counter].code - '0'.code) * powerOf(2, i))
-        counter += 1
-    }
-    println("\nThe Binary Number Entered: $num")
-    println("Converts Into the Decimal Number: ${newNum.sum()}\n")
 }
